019bd751686305221be637481b3145e1d2d3ffa5
LeeGyung-jin <0l055225757@gmail.com>
2022-07-19 01:27:38 +0900

    [p.434] 영화 이미지 등록/삭제하기

---
+++ src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -0,0 +1,44 @@
+package org.zerock.mreview.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.MovieImageDTO;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.MovieImage;
+import org.zerock.mreview.repository.MovieImageRepository;
+import org.zerock.mreview.repository.MovieRepository;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class MovieServiceImpl implements MovieService{
+
+    private final MovieRepository movieRepository;
+    private final MovieImageRepository imageRepository;
+
+    @Transactional
+    @Override
+    public Long register(MovieDTO movieDTO) {
+
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+        movieRepository.save(movie);
+
+        System.out.println("IMPLLL==========================");
+        System.out.println(movie);
+        System.out.println("==========================");
+
+        movieImageList.forEach(movieImage -> {
+            imageRepository.save(movieImage);
+        });
+        return movie.getMno();
+    }
+}
