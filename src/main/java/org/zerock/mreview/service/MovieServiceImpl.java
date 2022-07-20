5d75a277965df19cbde9c58c25c21ab897243041
LeeGyung-jin <0l055225757@gmail.com>
2022-07-20 17:03:59 +0900

    [p.446] 영화 리뷰 페이지 - 등록된 영화 제목, 리뷰수, 평점, 이미지 조회

--- src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
+++ src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -2,17 +2,24 @@ package org.zerock.mreview.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.dto.MovieDTO;
-import org.zerock.mreview.dto.MovieImageDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
+import org.zerock.mreview.dto.PageResultDTO;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 import org.zerock.mreview.repository.MovieImageRepository;
 import org.zerock.mreview.repository.MovieRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -40,5 +47,44 @@ public class MovieServiceImpl implements MovieService{
             imageRepository.save(movieImage);
         });
         return movie.getMno();
+    }
+
+    @Override
+    public PageResultDTO<MovieDTO, Object[]> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("mno").descending());
+        Page<Object[]> result = movieRepository.getListPage(pageable);
+        Function<Object[], MovieDTO> fn = (arr -> entitiesToDTO(
+                (Movie) arr[0],
+                (List<MovieImage>) (Arrays.asList((MovieImage) arr[1])),
+                (Double) arr[2],
+                (Long)arr[3])
+        );
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public MovieDTO getMovie(Long mno) {
+
+        List<Object[]> result = movieRepository.getMovieWithAll(mno);
+
+        Movie movie = (Movie) result.get(0)[0];                 // Movie 엔티티는 가장 앞에 존재한다. - 모든 Row가 동일한 값
+        List<MovieImage> movieImageList = new ArrayList<>();    // 영화의 이미지 개수만큼 MovieImage가 필요하다.
+
+        result.forEach(arr -> {
+            MovieImage movieImage = (MovieImage) arr[1];
+            movieImageList.add(movieImage);
+        });
+
+        Double avg = (Double) result.get(0)[2];    // 평균 평점 - 모든 Row가 동일한 값
+        Long reviewCnt = (Long) result.get(0)[3];   // 리뷰 개수 - 모든 Row가 동일한 값
+
+        return entitiesToDTO(movie, movieImageList, avg, reviewCnt);
+    }
+
+    @Override
+    public MovieDTO entitiesToDTO(Movie movie, List<MovieImage> movieImages, Double avg, Long reviewCnt) {
+        return MovieService.super.entitiesToDTO(movie, movieImages, avg, reviewCnt);
     }
 }
